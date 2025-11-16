e538c2222fc4c376d928d3057c086f94685fea88
Ana Nguyen <anax@stanford.edu>
2025-11-15 19:56:24 -0800

    New ghost prefabs

---
+++ Assets/Scripts/Matchmaking.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Matchmaking : MonoBehaviour
+{
+    private int lives = 3;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MakeMatchAccepted()
+    {
+        
+    }
+
+    public void MakeMatchRejected()
+    {
+        
+    }
+}
